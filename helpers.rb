d79bc34d9e812494f33c41e5584c9a58631d477f
Tony Summerville <tsummerville@rarestep.com>
2012-04-17 09:37:29 -0500

    Default layout and beginnings of index page

--- helpers.rb
+++ helpers.rb
@@ -1,3 +1,6 @@
 module FrankHelpers
-  # helpers go here
+  # Returns the HTML class for a navigation link, handling the logic to show the 'active' nav item.
+  def nav_link_class(link, active_nav_link = nil)
+    defined?(active_nav_link) && active_nav_link == link ? 'btn disabled' : ''
+  end
 end
